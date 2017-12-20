bf9ee88162ca988c65bcc5791d87c8b2d055a42a
tiagochst <tiagochst@gmail.com>
2017-12-19 22:59:36 -0200

    Fixes requested

--- R/TCGAbiolinks.data.R
+++ R/TCGAbiolinks.data.R
@@ -1,4 +1,4 @@
-#' @title Data for TCGAbiolinksGUI package.
+#' @title Auxiliary data for TCGAbiolinksGUI package.
 #' @description
 #' TCGAbiolinksGUI.data provide the necessary data for TCGAbiolinksGUI glioma classifier menu.
 #' @docType package
@@ -8,6 +8,7 @@
 #' \code{\link[TCGAbiolinksGUI.data]{maf.tumor}}, 
 #' \code{\link[TCGAbiolinksGUI.data]{GDCdisease}}
 #' @name TCGAbiolinksGUI.data
+#' @rdname TCGAbiolinksGUI-data 
 #' @exportPattern ^[[:alpha:]]+
 #' @keywords utilities
 #' @examples
@@ -19,79 +20,73 @@ NULL
 #' @keywords internal
 #' @name glioma.gcimp.model
 #' @format A train object 
+#' @rdname TCGAbiolinksGUI-data 
 #' @examples
-#' \dontrun{
 #' data("glioma.gcimp.model")
-#' }
 "glioma.gcimp.model"
 
 #' A train model for IDHmut DNA methylation signatures
 #' @docType data
 #' @keywords internal
+#' @rdname TCGAbiolinksGUI-data 
 #' @name glioma.idhmut.model
 #' @format A train object 
 #' @examples
-#' \dontrun{
 #' data("glioma.idhmut.model")
-#' }
 "glioma.idhmut.model"
 
 
 #' A train model for IDHwt DNA methylation signatures
 #' @docType data
+#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name glioma.idhwt.model
 #' @format A train object 
 #' @examples
-#' \dontrun{
 #' data("glioma.idhwt.model")
-#' }
 "glioma.idhwt.model"
 
 
 #' A train model for IDH DNA methylation signatures
 #' @docType data
+#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name glioma.idh.model
 #' @format A train object 
 #' @examples
-#' \dontrun{
 #' data("glioma.idh.model")
-#' }
 "glioma.idh.model"
 
 
 #' GDC projects list
 #' @docType data
+#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name GDCdisease
 #' @format A list with 39 elements
 #' @examples
-#' \dontrun{
 #' data("GDCdisease")
-#' }
 "GDCdisease"
 
 #' TCGA projects with MAF files
 #' @docType data
+#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name maf.tumor
 #' @format A list with 33 elements
 #' @examples
-#' \dontrun{
 #' data("maf.tumor")
-#' }
 "maf.tumor"
 
 
 #' List of probs that should be removed 
 #' from EPIC array due to different versions of the platform
+#' Source: https://support.illumina.com/downloads/infinium-methylationepic-v1-0-product-files.html
 #' @docType data
+#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name probes2rm
 #' @format A list with 977 elements
 #' @examples
-#' \dontrun{
 #' data("probes2rm")
-#' }
 "probes2rm"
