d58f60d7ea2aab8b206d0823c5d48308e0db8e94
tiagochst <tiagochst@gmail.com>
2017-12-28 09:48:58 -0200

    Update man

--- R/TCGAbiolinks.data.R
+++ R/TCGAbiolinks.data.R
@@ -4,27 +4,28 @@
 #'  It includes the following objects: 
 #'  
 #' \describe{
-#'  \item{\strong{glioma.gcimp.model}}{A train model for GCIMP DNA methylation signatures}
-#'  \item{\strong{glioma.idhwt.model}}{A train model for IDHwt DNA methylation signatures}
-#'  \item{\strong{glioma.idhmut.model}}{A train model for IDHmut DNA methylation signatures}
-#'  \item{\strong{glioma.idh.mode}}{A train model for IDH DNA methylation signatures}
-#'  \item{\strong{probes2rm}}{List of probes that should be removed from EPIC array due to different versions of the platform.\cr Source: https://support.illumina.com/downloads/infinium-methylationepic-v1-0-product-files.html }
-#'  \item{\strong{maf.tumor}}{TCGA projects with open MAF files retrieved from  the NCI's Genomic Data Commons (GDC). \cr Source: https://gdc-docs.nci.nih.gov/Data/Release_Notes/Manifests/GDC_open_MAFs_manifest.txt}
-#'  \item{\strong{GDCdisease}}{The NCI's Genomic Data Commons (GDC) projects list}
+#'  \item{\strong{\link[TCGAbiolinksGUI.data]{glioma.gcimp.model}}}{A train model for GCIMP DNA methylation signatures.}
+#'  \item{\strong{\link[TCGAbiolinksGUI.data]{glioma.idhwt.model}}}{A train model for IDHwt DNA methylation signatures}
+#'  \item{\strong{\link[TCGAbiolinksGUI.data]{glioma.idhmut.model}}}{A train model for IDHmut DNA methylation signatures}
+#'  \item{\strong{\link[TCGAbiolinksGUI.data]{glioma.idh.model}}}{A train model for IDH DNA methylation signatures}
+#'  \item{\strong{\link[TCGAbiolinksGUI.data]{probes2rm}}}{List of probes that should be removed from EPIC array due to different versions of the platform.\cr Source: https://support.illumina.com/downloads/infinium-methylationepic-v1-0-product-files.html }
+#'  \item{\strong{\link[TCGAbiolinksGUI.data]{maf.tumor}}}{TCGA projects with open MAF files retrieved from  the NCI's Genomic Data Commons (GDC). \cr Source: https://gdc-docs.nci.nih.gov/Data/Release_Notes/Manifests/GDC_open_MAFs_manifest.txt}
+#'  \item{\strong{\link[TCGAbiolinksGUI.data]{GDCdisease}}}{The NCI's Genomic Data Commons (GDC) projects list}
 #' }
 #' @docType package
 #' @name TCGAbiolinksGUI.data
 #' @rdname TCGAbiolinksGUI-data 
-#' @format A RF train model or a list.
-#' @return A RF train model or a list.
+#' @format NULL
+#' @return NULL
 #' @exportPattern ^[[:alpha:]]+
 #' @keywords utilities
 NULL
 
+#' @title gcimp RF model
+#' @description A RF model able to classify DNA methylation samples in to GCIMP groups
 #' @docType data
 #' @keywords internal
 #' @name glioma.gcimp.model
-#' @rdname TCGAbiolinksGUI-data 
 #' @format A random forest model with 276 samples and 145 predictors classifying into 3 classes
 #' @source RF model created from DNA methylation signatures retrieved 
 #' from \url{www.cell.com/cell/abstract/S0092-8674(15)01692-X}
@@ -32,9 +33,10 @@ NULL
 #' data("glioma.gcimp.model")
 "glioma.gcimp.model"
 
+#' @title IDHmut RF model
+#' @description A RF model able to classify DNA methylation samples in to IDHmut groups
 #' @docType data
 #' @keywords internal
-#' @rdname TCGAbiolinksGUI-data 
 #' @format A random forest model with 450 samples and 1216 predictors classifying into 3 classes
 #' @name glioma.idhmut.model
 #' @source RF model created from DNA methylation signatures retrieved 
@@ -43,9 +45,9 @@ NULL
 #' data("glioma.idhmut.model")
 "glioma.idhmut.model"
 
-
+#' @title IDHwt RF model
+#' @description A RF model able to classify DNA methylation samples in to IDHwt groups
 #' @docType data
-#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name glioma.idhwt.model
 #' @format A random forest model with 430 samples and 843 predictors classifying into 3 classes
@@ -55,9 +57,9 @@ NULL
 #' data("glioma.idhwt.model")
 "glioma.idhwt.model"
 
-
+#' @title IDH RF model
+#' @description A RF model able to classify DNA methylation samples in to IDH groups
 #' @docType data
-#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @format A random forest model with 880 samples and 1205 predictors classifying into 6 classes
 #' @name glioma.idh.model
@@ -67,9 +69,9 @@ NULL
 #' data("glioma.idh.model")
 "glioma.idh.model"
 
-
+#' @title GDC projects
+#' @description Contains all GDC projects with open data
 #' @docType data
-#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name GDCdisease
 #' @format A named list with 39 projects
@@ -78,8 +80,9 @@ NULL
 #' data("GDCdisease")
 "GDCdisease"
 
+#' @title GDC open MAF files 
+#' @description Contains the list of GDC project with open MAF files available
 #' @docType data
-#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name maf.tumor
 #' @format A named list with 33 tumors
@@ -88,8 +91,9 @@ NULL
 #' @source \url{https://gdc-docs.nci.nih.gov/Data/Release_Notes/Manifests/GDC_open_MAFs_manifest.txt}
 "maf.tumor"
 
+#' @title EPIC probes removed from newer versions
+#' @description EPIC probes removed from newer versions that should not be used in the analysis
 #' @docType data
-#' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name probes2rm
 #' @format A list with 977 probes
