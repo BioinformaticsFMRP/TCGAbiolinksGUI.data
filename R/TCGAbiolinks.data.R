811914fb8566809cc96250fefd1745beda3cf10b
tiagochst <tiagochst@gmail.com>
2017-12-28 02:39:09 -0200

    Typo fixed

--- R/TCGAbiolinks.data.R
+++ R/TCGAbiolinks.data.R
@@ -1,7 +1,7 @@
 #' @title Auxiliary data for TCGAbiolinksGUI package.
 #' @description
 #' \strong{Package:} TCGAbiolinksGUI.data provide the necessary data for TCGAbiolinksGUI glioma classifier menu.
-#'  It includes the fololowing objects: 
+#'  It includes the following objects: 
 #'  
 #' \describe{
 #'  \item{\strong{glioma.gcimp.model}}{A train model for GCIMP DNA methylation signatures}
@@ -25,6 +25,9 @@ NULL
 #' @keywords internal
 #' @name glioma.gcimp.model
 #' @rdname TCGAbiolinksGUI-data 
+#' @format A random forest model with 276 samples and 145 predictors classifying into 3 classes
+#' @source RF model created from DNA methylation signatures retrieved 
+#' from \url{www.cell.com/cell/abstract/S0092-8674(15)01692-X}
 #' @examples
 #' data("glioma.gcimp.model")
 "glioma.gcimp.model"
@@ -32,7 +35,10 @@ NULL
 #' @docType data
 #' @keywords internal
 #' @rdname TCGAbiolinksGUI-data 
+#' @format A random forest model with 450 samples and 1216 predictors classifying into 3 classes
 #' @name glioma.idhmut.model
+#' @source RF model created from DNA methylation signatures retrieved 
+#' from \url{www.cell.com/cell/abstract/S0092-8674(15)01692-X}
 #' @examples
 #' data("glioma.idhmut.model")
 "glioma.idhmut.model"
@@ -42,6 +48,9 @@ NULL
 #' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name glioma.idhwt.model
+#' @format A random forest model with 430 samples and 843 predictors classifying into 3 classes
+#' @source RF model created from DNA methylation signatures retrieved 
+#' from \url{www.cell.com/cell/abstract/S0092-8674(15)01692-X}
 #' @examples
 #' data("glioma.idhwt.model")
 "glioma.idhwt.model"
@@ -50,7 +59,10 @@ NULL
 #' @docType data
 #' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
+#' @format A random forest model with 880 samples and 1205 predictors classifying into 6 classes
 #' @name glioma.idh.model
+#' @source RF model created from DNA methylation signatures retrieved 
+#' from \url{www.cell.com/cell/abstract/S0092-8674(15)01692-X}
 #' @examples
 #' data("glioma.idh.model")
 "glioma.idh.model"
@@ -60,6 +72,8 @@ NULL
 #' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name GDCdisease
+#' @format A named list with 39 projects
+#' @source Retrivied from GDC API
 #' @examples
 #' data("GDCdisease")
 "GDCdisease"
@@ -68,14 +82,18 @@ NULL
 #' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name maf.tumor
+#' @format A named list with 33 tumors
 #' @examples
 #' data("maf.tumor")
+#' @source \url{https://gdc-docs.nci.nih.gov/Data/Release_Notes/Manifests/GDC_open_MAFs_manifest.txt}
 "maf.tumor"
 
 #' @docType data
 #' @rdname TCGAbiolinksGUI-data 
 #' @keywords internal
 #' @name probes2rm
+#' @format A list with 977 probes
+#' @source \url{https://support.illumina.com/downloads/infinium-methylationepic-v1-0-product-files.html}
 #' @examples
 #' data("probes2rm")
 "probes2rm"
